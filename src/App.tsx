74654947d52fc50832a5cb6d9d94be781e28c7d1
jhraju <jhraju890@gmail.com>
2024-04-24 21:12:21 +0600

    added explore ref and controlled and uncontrolled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 //  import SimpleForm from './components/SimpleForm/SimpleForm.jsx'
- import StatefulForm from './components/StatefulForm/StatefulForm.jsx'
+//  import StatefulForm from './components/StatefulForm/StatefulForm.jsx'
+import RefForm from './components/RefForm/RefForm.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,7 +11,8 @@ function App() {
       
       <h1>Vite + React</h1>
     {/* <SimpleForm></SimpleForm>   */}
-   <StatefulForm></StatefulForm>
+   {/* <StatefulForm></StatefulForm> */}
+   <RefForm></RefForm>
     </>
   )
 }
